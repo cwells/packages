78193658405a118d1604021f486900e8c89d2e2f
Cliff Wells <cwells@focusvision.com>
2018-08-29 00:39:25 -0700

    rpm-summary

--- elixir/entrypoint.sh
+++ elixir/entrypoint.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
 VERSION=$1
+ITERATION=1
 PREFIX=/opt/elixir
 
 curl -s http://packages.erlang-solutions.com/rpm/centos/erlang_solutions.repo -o /etc/yum.repos.d/erlang_solutions.repo
 yum -y install esl-erlang
 
-git clone --branch v${VERSION} --depth 1 https://github.com/elixir-lang/elixir.git $PREFIX
-(cd $PREFIX && make clean && make)
+git clone --branch v$VERSION --depth 1 https://github.com/elixir-lang/elixir.git $PREFIX
+(cd $PREFIX && make clean && make -j4)
 
 cat << EOF > /tmp/after-install
 ln -s $PREFIX/bin/{iex,mix,elixir,elixirc} /usr/local/bin
@@ -18,13 +19,13 @@ rm -f /usr/local/bin/{iex,mix,elixir,elixirc}
 EOF
 
 fpm --input-type dir --output-type rpm --package /packages \
-  --name elixir --version ${VERSION} --iteration 1 \
+  --name elixir --version $VERSION --iteration $ITERATION \
   --depends esl-erlang \
-  --summary 'The Elixir programming language' \
+  --rpm-summary 'The Elixir programming language' \
   --description 'Elixir is a dynamic, functional language designed for building scalable and maintainable applications.' \
   --url 'https://elixir-lang.org/' \
-  --vendor 'FocusVision Worldwide, Inc' \
   --license 'Apache 2.0' \
   --after-install /tmp/after-install \
   --before-remove /tmp/before-remove \
+  --force \
   $PREFIX
