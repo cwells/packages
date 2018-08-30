7fea7cc249c1801650bd69ff110aaed438dd1d1c
Cliff Wells <cwells@focusvision.com>
2018-08-30 08:05:23 -0700

    move cmake build stuff into c7.sh

--- julia/centos7.sh
+++ julia/centos7.sh
@@ -3,10 +3,15 @@ DISTRO_GFORTRAN="gfortran"
 function install_cmake() {
   yum -y -q -e 0 remove cmake
 
+  CMAKE_VERSION=${CMAKE_VERSION:='3.12.1'}
+  CMAKE_MAJOR=$( version_split $CMAKE_VERSION 0 )
+  CMAKE_MINOR=$( version_split $CMAKE_VERSION 1 )
+  CMAKE_SHORT="${CMAKE_MAJOR}.${CMAKE_MINOR}"
+
   ( # julia requires recent cmake
     cd /usr/local/src
-    curl "https://cmake.org/files/v${CMAKE_MAJOR}.${CMAKE_MINOR}/cmake-${CMAKE}.tar.gz" -o cmake.tar.gz
+    curl "https://cmake.org/files/v${CMAKE_MAJOR}.${CMAKE_MINOR}/cmake-${CMAKE_VERSION}.tar.gz" -o cmake.tar.gz
     tar zxvf cmake.tar.gz
-    ( cd "cmake-${CMAKE}" && ./bootstrap --prefix=/usr/local && make -j4 && make install )
+    ( cd "cmake-${CMAKE_VERSION}" && ./bootstrap --prefix=/usr/local && make -j4 && make install )
   )
 }
