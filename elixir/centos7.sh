892551293becd3700699955f9b7084161018bf31
Cliff Wells <cwells@focusvision.com>
2018-08-30 01:18:53 -0700

    redirect yum to devnull so it doesn't mangle function value

--- elixir/centos7.sh
+++ elixir/centos7.sh
@@ -1,5 +1,5 @@
 function install_erlang() {
-  yum -y -q -e 0 install https://packages.erlang-solutions.com/erlang-solutions-1.0-1.noarch.rpm
-  yum -y -q -e 0 install esl-erlang
+  yum -y -q -e 0 install https://packages.erlang-solutions.com/erlang-solutions-1.0-1.noarch.rpm &> /dev/null
+  yum -y -q -e 0 install esl-erlang &> /dev/null
   echo "esl-erlang"
 }
--- elixir/fedora28.sh
+++ elixir/fedora28.sh
@@ -1,4 +1,4 @@
 function install_erlang() {
-  yum -y -q -e 0 install erlang
+  yum -y -q -e 0 install erlang &> /dev/null
   echo "erlang"
 }
