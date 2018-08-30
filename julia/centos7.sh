531e7a551f4096791b9ef77deabad17a43f64a02
Cliff Wells <cwells@focusvision.com>
2018-08-30 01:51:48 -0700

    don't build cmake on f28

--- julia/centos7.sh
+++ julia/centos7.sh
@@ -1,1 +1,12 @@
 GFORTRAN="gfortran"
+
+function install_cmake() {
+  yum -y -q -e 0 remove cmake
+
+  ( # julia requires recent cmake
+    cd /usr/local/src
+    curl "https://cmake.org/files/v${CMAKE_MAJOR}.${CMAKE_MINOR}/cmake-${CMAKE}.tar.gz" -o cmake.tar.gz
+    tar zxvf cmake.tar.gz
+    ( cd "cmake-${CMAKE}" && ./bootstrap --prefix=/usr/local && make -j4 && make install )
+  )
+}
--- julia/fedora28.sh
+++ julia/fedora28.sh
@@ -1,1 +1,5 @@
 GFORTRAN="gcc-gfortran"
+
+function install_cmake() {
+  yum -y -q -e 0 install cmake
+}
